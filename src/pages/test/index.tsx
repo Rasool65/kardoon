24d17f24e6e0bee20c5aa273a3e8cd383c37aa74
Rasool <Aghajani.Rasool@Gmail.com>
2022-06-16 23:47:51 +0430

    create test

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent, useEffect, useState } from 'react';
+
+import IPageProps from '../../configs/routerConfig/IPageProps';
+
+
+const test: FunctionComponent<IPageProps> = (props) => {
+
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default test;
